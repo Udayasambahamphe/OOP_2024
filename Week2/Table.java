dc19d5e7e5dcaba356defde5e1e03cd9b6f619e5
Udayasambahamphe <subbaudaya094@gmail.com>
2024-03-14 12:17:14 +0545

    Weekly Tasks

---
+++ Week2/Table.java
@@ -0,0 +1,40 @@
+public class Table {
+    public static void main(String[] args) {
+        System.out.println("///////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+        System.out.println("---   Student Points       ---");
+        System.out.println("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\///////////////////");
+
+        System.out.println("Name\t\tLab\tBonus\tTotal");
+        System.out.println("----\t\t---\t-----\t-----");
+
+        String name1 = "Udaya Sambahamphe";
+        int labPoints1 = 42;
+        int bonusPoints1 = 5;
+        int total1 = labPoints1 + bonusPoints1;
+        System.out.println(name1 + "\t\t" + labPoints1 + "\t" + bonusPoints1 + "\t" + total1);
+
+        String name2 = "Ram kumar";
+        int labPoints2 = 50;
+        int bonusPoints2 = 8;
+        int total2 = labPoints2 + bonusPoints2;
+        System.out.println(name2 + "\t\t" + labPoints2 + "\t" + bonusPoints2 + "\t" + total2);
+
+        String name3 = "shyam bahadur";
+        int labPoints3 = 39;
+        int bonusPoints3 = 7;
+        int total3 = labPoints3 + bonusPoints3;
+        System.out.println(name3 + "\t" + labPoints3 + "\t" + bonusPoints3 + "\t" + total3);
+
+        String name4 = "hari limbu";
+        int labPoints4 = 24;
+        int bonusPoints4 = 8;
+        int total4 = labPoints4 + bonusPoints4;
+        System.out.println(name4 + "\t\t" + labPoints4 + "\t" + bonusPoints4 + "\t" + total4);
+
+        String name5 = "gyan subba";
+        int labPoints5 = 36;
+        int bonusPoints5 = 5;
+        int total5 = labPoints5 + bonusPoints5;
+        System.out.println(name5 + "\t\t" + labPoints5 + "\t" + bonusPoints5 + "\t" + total5);
+    }
+}
