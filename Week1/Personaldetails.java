f822a49d13ba7087525e4b9fa0f1b89050de94bf
Udayasambahamphe <subbaudaya094@gmail.com>
2024-03-14 12:13:15 +0545

    Weekly Tasks

---
+++ Week1/Personaldetails.java
@@ -0,0 +1,16 @@
+
+public class PersonalDetails {
+    public static void main(String args[]) {
+
+        String Name = "Udaya Sambahamphe";
+        int age = 21;
+        int Phone = 9700000000;
+        String address = "lalitpur";
+
+        System.out.println(Name);
+        System.out.println(age);
+        System.out.println(Phone);
+        System.out.println(address);
+
+    }
+}
